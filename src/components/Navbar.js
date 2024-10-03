cce88b8a9ee9a19dc830a9a954c2adec0b618170
Spencer K Chuong <spencerchuong0@gmail.com>
2024-10-02 21:21:01 -0700

    Improving Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Navbar.css';
+import ResumePDF from '../assets/Spencer Chuong\'s Resume.pdf'; // Adjust the path if needed
 
 const Navbar = () => {
     return (
@@ -11,7 +12,7 @@ const Navbar = () => {
                 <a href="#section-two">About</a> {/* About leads to the second section */}
                 <a href="#section-three">Projects</a> {/* Projects leads to the third section */}
                 <a href="#section-four">Contact</a> {/* Contact leads to the fourth section */}
-                <a href="#resume" className="resume-link">RESUME</a> {/* Resume link remains as is */}
+                <a href={ResumePDF} target="_blank" rel="noopener noreferrer" className="resume-link">RESUME</a>
             </div>
         </nav>
     );
